cde18882664775ac57e724b67c740b65937d4e97
Hugh He <plushugh@gmail.com>
2023-07-03 15:33:23 +0800

    fix ignore + Vec reference in bin_search

--- bin_search/src/main.rs
+++ bin_search/src/main.rs
@@ -1,15 +1,14 @@
 fn main() {
-    let test = vec![1,4,5,99,567,3123,7956,11036];
+    let test = vec![1, 4, 5, 99, 567, 3123, 7956, 11036];
     let find = 4;
 
-    if let Some(res) = binary_search(test, find) {
+    if let Some(res) = binary_search(&test, find) {
         assert_eq!(res, 1);
         println!("The index of '4' is: {}", res);
     }
-
 }
 
-fn binary_search<T: Ord + Eq + Copy>(vec: [T], item: T) -> Option<usize> {
+fn binary_search<T: Ord + Eq + Copy>(vec: &Vec<T>, item: T) -> Option<usize> {
     let mut low = 0;
     let mut high = vec.len() - 1;
 
